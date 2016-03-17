e33030fb64f74683f4304d12832b3727100c6fea
tao <s13218to@sfc.keio.ac.jp>
2016-03-17 11:30:08 +0900

    完成系？

--- test4/main.js
+++ test4/main.js
@@ -1,44 +1,89 @@
-var dataList = [300,100,200];
-
-var A = (function(){
-  function A(runAction){
+var test,
     xScale = d3.scale.linear()
       .domain([0,300])
       .range([0,300]),
-    XScalePos = dataList.length * 25,
-    height = 20,
-    space = 5,
-    this.runAction = runAction; //コンストラクタ部分
+    dataSet = {
+      "id": "graph",
+      "dataList1": [300,100,50,230,25],
+      "height": 20,
+      "space": 5
+    };
+    dataSet2 = {
+      "id": "graph",
+      "dataList2": [100,120,80,200,90],
+      "height": 20,
+      "space": 5
+    };
+
+
+
+var BarGraph = (function(){
+  function BarGraph(dataSet){
+    this.dataSet = dataSet; //コンストラクタ部分
   }
 
-  A.prototype.run = function() {
-    d3.select("#graph")
+  BarGraph.prototype.create = function() {
+    var yPos = this.dataSet.height + this.dataSet.space,
+        XScalePos = this.dataSet.dataList1.length * yPos;
+
+    d3.select("#" + this.dataSet.id)
       .append("g")
       .attr("class","axis")
       .attr("transform", "translate(0," + XScalePos + ")")
       .call(d3.svg.axis().scale(xScale).orient("bottom"));
-    d3.select("#graph")
+    d3.select("#" + this.dataSet.id)
       .selectAll("rect")
-      .data(this.runAction)
+      .data(this.dataSet.dataList1)
       .enter()
       .append("rect")
       .attr("y",function(d,i){
-        return (height + space) * i;
+        return yPos * i;
       })
       .attr("x",0)
-      .attr("height",height)
-      .attr("width",0)
+      .attr("height",this.dataSet.height)
       .attr("width",function(d,i){
         return d;
       });
   };
-  return A;
+
+  BarGraph.prototype.change1 = function(){
+    d3.select("#" + this.dataSet.id)
+      .selectAll("rect")
+      .data(this.dataSet.dataList1)
+      .transition()
+      .attr("x",0)
+      .attr("width",function(d,i){
+        return d;
+      });
+  };
+
+  BarGraph.prototype.change2 = function(){
+    d3.select("#" + this.dataSet.id)
+      .selectAll("rect")
+      .data(this.dataSet.dataList2)
+      .transition()
+      .attr("x",0)
+      .attr("width",function(d,i){
+        return d;
+      });
+  };
+
+  return BarGraph;
 })();
 
-var test = new A(dataList);
-
 d3.select("#create").on("click",function(){
-  test.run();
-})
+  test = new BarGraph(dataSet);
+  test.create();
+});
 
-console.log(height)
+d3.select("#updata1").on("click",function(){
+  test = new BarGraph(dataSet);
+  test.change1();
+});
+
+d3.select("#updata2").on("click",function(){
+  test = new BarGraph(dataSet2);
+  test.change2();
+});
+
+
